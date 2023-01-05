9312ff6cb2539560b9aa1c502db22e6a7558cb19
miecz <miecz@LAPTOP-UVM3H6HN>
2023-01-05 13:36:17 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectRef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
